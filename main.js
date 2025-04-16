a8c0ee2585e3a6d11cf33e6fdb38a6c47ff3a4ea
Phatchara "Tsingtoh" Khongsamphan <tsingtoh@gmail.com>
2025-04-17 00:17:12 +0700

    more or less completed the design

--- main.js
+++ main.js
@@ -4,19 +4,54 @@ window.onload = function() {
 
 function Head() {
     document.getElementById(`head`).insertAdjacentHTML(`beforeend`, 
-        `<link rel="icon" type="image/x-icon" href="./css/icon/favicon.png">`);
+        `<link rel="icon" type="image/x-icon" href="/css/fonts/favicon.png">`);
 }
 
 function Menu() {
 
     document.getElementById(`body`).insertAdjacentHTML(`afterbegin`, 
         `<nav id="menu">
-        <a href="./index.html">Home</a>
-        <a href="./articles.html">Articles</a>
-        <a href="./about.html">About</a>
+        <a id="articles" href="/articles.html">Articles</a>
+        -
+        <a id="portfolio" href="/index.html">Portfolio</a>
+        
+            <div>
+            <button>+</button>
+            <button>+</button>
+            <button>-</button>
+            </div>
+
         </nav>`);
     }
     
 
-    
+function Foot() {
 
+    document.getElementById(`body`).insertAdjacentHTML(`beforeend`, 
+        `<div class="foot-class">
+            <footer>
+
+                <p>GET IN TOUCH : <a href="https://www.linkedin.com/in/tsingtoh/" target="_blank">LinkedIn</a> | <a>Email</a></p>
+                <p><strong>ALL RIGHTS RESERVED © 2025 TSINGTOH</strong></p>
+
+            </footer>
+        </div>
+        `);
+}
+
+
+function WebDev() {
+    document.getElementById(`web-dev`).classList.add(`selected`);
+    document.getElementById(`writer`).classList.remove(`selected`);
+
+    document.getElementById(`writing`).style.display = `none`;
+    document.getElementById(`web-design`).style.display = `flex`;
+}
+
+function Writer() {
+    document.getElementById(`writer`).classList.add(`selected`);
+    document.getElementById(`web-dev`).classList.remove(`selected`);
+
+    document.getElementById(`web-design`).style.display = `none`;
+    document.getElementById(`writing`).style.display = `block`;
+}
